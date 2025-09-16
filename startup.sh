1b41accd7a64e23dabe3d9f4b0f0fffba9fc3af4
jisun118 <jisun890421@gmail.com>
2025-09-16 13:16:11 +0900

    Update startup.sh
    startup 수정

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Streamlit 앱을 특정 포트에서 실행
-python -m streamlit run EmailAnalyzer.py --server.port=$PORT --server.address=0.0.0.0 --server.enableCORS=false --server.enableXsrfProtection=false
+streamlit run ./EmailAnalyzer.py
